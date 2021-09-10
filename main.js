3d5c5384f1712bf4ab01b33d6f0f97d5a282478a
kadowning1 <kadowning1@gmail.com>
2021-09-10 09:27:34 -0400

    finish silly story

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
+/*copied code*/
 
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 let insertX = [
@@ -23,34 +24,34 @@ let insertZ = [
     'melted into a puddle on the sidewalk',
     'turned into a slug and crawled away'];
 
-
+/*creating variables*/
 
 randomize.addEventListener('click', result);
 
 function result() {
     let newStory = storyText;
     let xItem = randomValueFromArray(insertX);
-    let yItem = randomValueFromArray(insertY);
+    let yItem = randomValueFromArray(insertY); /*creating new variables*/
     let zItem = randomValueFromArray(insertZ);
 
-    newStory = newStory.replace(':insertx:',xItem);
+    newStory = newStory.replace(':insertx:',xItem); /*replacing placeholders*/
     newStory = newStory.replace(':insertx:',xItem);
     newStory = newStory.replace(':inserty:',yItem);
     newStory = newStory.replace(':insertz:',zItem);
 
   if(customName.value !== '') {
     let name = customName.value;
-    newStory = newStory.replace('Bob', name)
+    newStory = newStory.replace('Bob', name) /*replacing bob*/
 
   }
 
   if(document.getElementById("uk").checked) {
     let weight = Math.round(300/14) + ' stone';
-    let temperature =  Math.round((94-32) * 5 / 9) + ' centrigrade';
+    let temperature =  Math.round((94-32) * 5 / 9) + ' centrigrade'; /*converting amounts*/
     newStory = newStory.replace('94 fahrenheit',temperature)
     newStory = newStory.replace('300 pounds',weight)
   }
 
-  story.textContent = newStory;
+  story.textContent = newStory; /*making equal to new story*/
   story.style.visibility = 'visible';
 }
